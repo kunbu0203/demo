b5f0856586445625d84de2fc2aea32225f3007ba
kunbu0203 <59350831+kunbu0203@users.noreply.github.com>
2021-09-03 11:01:50 +0800

    Alife: 0903

--- Alife/content/js/drop.js
+++ Alife/content/js/drop.js
@@ -1,23 +1,26 @@
 var tl = gsap.timeline({
-  onComplete: myFunction
+    onComplete: myFunction
 });
-var H = $(window).height() - 160,
-    fromH = H - 40;
+// var H = $(window).height() - 160,
+//     fromH = H - 40;
+var wineH = $(window).width() * 0.05,
+    H = $(window).height() - (wineH * 1.7),
+    fromH = H - (wineH * 0.4);
 tl.to('.wine', {
-  opacity: 1,
-  duration: 0.4
+    opacity: 1,
+    duration: 0.4
 }).to('.wine', {
-  ease: 'bounce.out',
-  y: 'random(' + fromH + ', ' + H + ')',
-  rotation: 'random(-180, 180)',
-  duration: 2,
-  stagger: {
-    amount: 0.4,
-    from: "random"
-  }
+    ease: 'bounce.out',
+    y: 'random(' + fromH + ', ' + H + ')',
+    rotation: 'random(-180, 180)',
+    duration: 2,
+    stagger: {
+        amount: 0.4,
+        from: "random"
+    }
 });
 tl.pause();
 
 function myFunction() {
-  tl.kill();
+    tl.kill();
 }
