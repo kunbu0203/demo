cb67480e4abe1616bbe8b918341b3f3776e55d21
kunbu0203 <59350831+kunbu0203@users.noreply.github.com>
2021-09-24 08:42:33 +0800

    asus 0924

--- asus/content/js/main.js
+++ asus/content/js/main.js
@@ -19,4 +19,70 @@ $(function () {
       $('[data-share]').addClass('fadeIn');
     }
   }).trigger('scroll');
+  var swiperTop = new Swiper('[data-slider-top]', {
+    effect: 'fade',
+    allowTouchMove: false
+  });
+  var swiperBottom = new Swiper('[data-slider-bottom]', {
+    loop: true,
+    loopAdditionalSlides: 4,
+    slidesPerView: 4,
+    breakpoints: {
+      1023: {
+        loopAdditionalSlides: 3,
+        slidesPerView: 3
+      }
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  });
+  var swiperCard = new Swiper('[data-slider-card]', {
+    centeredSlides: true,
+    slidesPerView: 5.5,
+    breakpoints: {
+      1023: {
+        centeredSlides: false,
+        slidesPerView: 2
+      },
+      767: {
+        centeredSlides: false,
+        slidesPerView: 1
+      }
+    },
+    navigation: {
+      nextEl: '.swiper-card-next',
+      prevEl: '.swiper-card-prev'
+    }
+  });
+  var swiperPopup = new Swiper('[data-slider-popup]', {
+    slidesPerView: 1,
+    navigation: {
+      nextEl: '.swiper-popup-next',
+      prevEl: '.swiper-popup-prev'
+    }
+  });
+  swiperBottom.on('slideChange', function () {
+    swiperTop.slideTo(swiperBottom.realIndex, 0);
+  });
+  swiperCard.on('slideChange', function () {
+    $('.swiper-card-index').text(swiperCard.realIndex + 1);
+  });
+  swiperPopup.on('slideChange', function () {
+    setTimeout(function () {
+      swiperBottom.slideTo(swiperPopup.realIndex, 500);
+    }, 500);
+    $('.swiper-popup-index').text(swiperPopup.realIndex + 1);
+  });
+  $('[data-popup-action]').on('click', function (e) {
+    e.preventDefault();
+    $('[data-popup]').removeClass('hide');
+    var val = $(this).data('popup-action');
+    swiperPopup.slideTo(val - 1, 0);
+  });
+  $('[data-close-popup]').on('click', function (e) {
+    e.preventDefault();
+    $('[data-popup]').addClass('hide');
+  });
 });
