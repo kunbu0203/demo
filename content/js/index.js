63881c0f454f3434519fde0682fcd6e5ac05e56f
kunbu0203 <59350831+kunbu0203@users.noreply.github.com>
2022-05-12 23:43:47 +0800

    Delete content directory

--- content/js/index.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  // 略過動畫
-  $('[data-banner-skip]').on('click', function (e) {
-    e.preventDefault();
-    $('[data-banner]').addClass('animationStop');
-  });
-});
--- content/js/product.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  // 詳細描述
-  $('[data-collapse-btn]').on('click', function (e) {
-    e.preventDefault();
-    $('[data-collapse]').toggleClass('isOpen');
-  });
-});
