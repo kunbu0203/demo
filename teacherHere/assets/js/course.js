a9fe211eb8eeea7a9083ece6a1c7b56c6bc19d41
kunbu0203 <59350831+kunbu0203@users.noreply.github.com>
2023-07-16 18:43:25 +0800

    Delete teacherHere directory

--- teacherHere/assets/js/course.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  $('[data-anchor-btn]').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({
-      scrollTop: $('[data-anchor-block="' + $(this).data('anchor-btn') + '"]').offset().top - 100
-    }, 300);
-  });
-});
