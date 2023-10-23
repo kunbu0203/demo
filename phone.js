9611706a29810375dc8fda9b2071e415e7fa6d34
kunbu0203 <59350831+kunbu0203@users.noreply.github.com>
2023-10-23 23:54:22 +0800

    Add files via upload

---
+++ phone.js
@@ -0,0 +1,36 @@
+$('#btn').on('click', onClick());
+
+function onClick() {
+  if (typeof DeviceMotionEvent.requestPermission === 'function') {
+    // Handle iOS 13+ devices.
+    DeviceMotionEvent.requestPermission().then(function (state) {
+      console.log(state);
+      alert(state);
+
+      if (state === 'granted') {
+        window.addEventListener('devicemotion', function (event) {
+          console.log('handleOrientation', event);
+          var alpha = event.alpha;
+          var beta = event.beta;
+          var gamma = event.gamma;
+          console.log(beta);
+          $('#beta').text(beta);
+          $('#gamma').text(gamma);
+        });
+      } else {
+        console.error('Request to access the orientation was rejected');
+      }
+    }).catch(console.error);
+  } else {
+    // Handle regular non iOS 13+ devices.
+    window.addEventListener('devicemotion', function (event) {
+      console.log('handleOrientation', event);
+      var alpha = event.alpha;
+      var beta = event.beta;
+      var gamma = event.gamma;
+      console.log(beta);
+      $('#beta').text(beta);
+      $('#gamma').text(gamma);
+    });
+  }
+}
